38959f479aac5dcd0407a4cf15ce464474232aae
Johnata Davi <infojohnata@gmail.com>
2020-01-08 21:56:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-import pygame as pg
-pg.init()
-display = pg.display.set_mode([1280, 720])
-pg.display.set_caption("Jogo de corno 2D")
+# https://www.pygame.org/docs/
+import pygame
+pygame.init()
+display = pygame.display.set_mode([1280, 720])
+pygame.display.set_caption("Jogo de corno 2D")
 
+# Objects
+rect = pygame.Rect(540, 310, 200, 100)
+
+clock = pygame.time.Clock();
 gameLoop = True
+speed = 10
 while gameLoop:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    # Framerate
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             gameLoop = False
-            
-    pg.display.update()
+        '''
+        elif event.type == pygame.KEYDOWN:
+            if  event.key == pygame.K_w:
+                rect[1] -= 50
+            if  event.key == pygame.K_a:
+                rect[0] -= 50
+            if  event.key == pygame.K_s:
+                rect[1] += 50
+            if  event.key == pygame.K_d:
+                rect[0] += 50
+        '''
+    keys = pygame.key.get_pressed();
+    if keys[pygame.K_w]:
+        rect[1] -= speed
+    if keys[pygame.K_a]:
+        rect[0] -= speed
+    if keys[pygame.K_s]:
+        rect[1] += speed
+    if keys[pygame.K_d]:  
+        rect[0] += speed
+    # Draw...
+    display.fill([10, 10, 10])
+    pygame.draw.rect(display, [0,0,255], rect);
+    pygame.display.update()
